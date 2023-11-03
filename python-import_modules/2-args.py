a8e9a20f52a41a0153a189e0f5ff187520ae9d45
jmugisha1 <j.mugisha1@alustudent.com>
2023-11-03 18:52:03 +0200

    python

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     else:
         print(len(argv) - 1, "arguments:")
         for count, i in enumerate(argv[1:]):
-            print(f'{count+1}: {
+            print(f'{count+1}: {i}')
