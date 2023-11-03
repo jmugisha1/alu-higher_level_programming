5d52fd953daa9c33a01057e5a69202f8d0303504
jmugisha1 <j.mugisha1@alustudent.com>
2023-11-03 18:49:20 +0200

    python

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    if (len(argv) - 1) == 0:
+        print("0 arguments.")
+    elif (len(argv) - 1) == 1:
+        print(len(argv) - 1, "argument:")
+        print("1:", argv[1])
+    else:
+        print(len(argv) - 1, "arguments:")
+        for count, i in enumerate(argv[1:]):
+            print(f'{count+1}: {
