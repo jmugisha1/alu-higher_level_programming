291aa70d698ec3b60928545cdc83f217d7306d01
jmugisha1 <j.mugisha1@alustudent.com>
2023-11-03 11:49:30 +0200

    loops

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,20 +5,19 @@ number = random.randint(-10000, 10000)
 number_str = str(number)
 last_digit_str = number_str[-1]
 last_digit_int = int(last_digit_str)
-if last_digit_int == 0:
-    print(f"Last digit of {number:d} is {last_digit_int:d} and is 0")
-elif number > 0 and last_digit_int > 5:
-    print(f"Last digit of {number:d} is {last_digit_int:d} and is greater than 5")
-elif number > 0 and last_digit_int < 6:
-    print(
-        f"Last digit of {number:d} is {last_digit_int:d} and is less than 6 and not 0"
-    )
-elif number < 0 and last_digit_int > 5:
-    print(
-        f"Last digit of {number:d} is -{last_digit_int:d} and is less than 6 and not 0"
-    )
-elif number < 0 and last_digit_int < 6:
-    print(
-        f"Last digit of {number:d} is -{last_digit_int:d} and is less than 6 and not 0"
-    )
 
+if last_digit_int == 0:
+    print(f"Last digit of {number} is 
+          {last_digit_int} and is 0")
+elif number > 0 and last_digit_int > 5:
+    print(f"Last digit of {number} is 
+          {last_digit_int} and is greater than 5")
+elif number > 0 and last_digit_int < 6:
+    print(f"Last digit of {number} is 
+          {last_digit_int} and is less than 6 and not 0")
+elif number < 0 and last_digit_int > 5:
+    print(f"Last digit of {number} is 
+          -{last_digit_int} and is less than 6 and not 0")
+elif number < 0 and last_digit_int < 6:
+    print(f"Last digit of {number} is 
+          -{last_digit_int} and is less than 6 and not 0")
