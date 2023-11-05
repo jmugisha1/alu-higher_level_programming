6f154eb1e9cd81dbdc220f2bded8827c648a8bcf
jmugisha1 <j.mugisha1@alustudent.com>
2023-11-05 23:18:10 +0200

    python-data_structures

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
 my_list = [1, 2, 3]
-def print_list_integer(my_list=[]):
-    for item in my_list:
+
+
+def print_list_integer(nums):
+    for item in nums:
         print("{:d}".format(item))
     return
 
+
 print_list_integer(my_list)
+
