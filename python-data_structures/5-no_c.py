f6cdd847f89dbce35d48db63c7dae27e3c615d57
jmugisha1 <j.mugisha1@alustudent.com>
2023-11-07 21:38:30 +0200

    python-data_structures

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     list_no_c = []
     list_str = list(my_string)
     for char in list_str:
-        if char != "c" and char != "C" in list_str:
+        if char != "c" and char != "C":
             list_no_c.append(char)
     return ''.join(list_no_c)
