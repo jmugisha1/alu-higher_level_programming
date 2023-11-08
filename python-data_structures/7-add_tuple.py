9f15156d40646c6eb7396cede8ba210b104d4974
jmugisha1 <j.mugisha1@alustudent.com>
2023-11-08 14:29:22 +0200

    python-data_structures

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a_list = list(tuple_a)
-    tuple_b_list = list(tuple_b)
-
-    if len(tuple_a_list) < 1 and len(tuple_b_list) < 1:
-        tuple_a_list.append(0)
-        tuple_b_list.append(0)
-
-    added_tuple_list = [tuple_a_list[0] + tuple_b_list[0]] +\
-    [tuple_a_list[1] + tuple_b_list[1]]
-
-    return tuple(added_tuple_list)
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
