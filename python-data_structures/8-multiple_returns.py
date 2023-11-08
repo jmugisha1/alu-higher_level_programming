ebf04b17f0b28f9b93c8924ff30c3520e6d443a4
jmugisha1 <j.mugisha1@alustudent.com>
2023-11-08 14:52:29 +0200

    python-data_structures

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == '':
-        return None
-    else:
+    if sentence:
         length = len(sentence)
         first = sentence[0]
         print("Length: {:d} - First character: {}".format(length, first))
+    else:
+        return None
