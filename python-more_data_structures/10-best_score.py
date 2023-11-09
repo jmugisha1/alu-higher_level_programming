ac25f000eebec0806d50498b716c5dfac03d075d
jmugisha1 <j.mugisha1@alustudent.com>
2023-11-09 23:19:57 +0200

    python-more_data_structures

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0 or a_dictionary is None:
+    if  a_dictionary is None or len(a_dictionary) == 0:
         return None
     else:
         max_score_list = []
