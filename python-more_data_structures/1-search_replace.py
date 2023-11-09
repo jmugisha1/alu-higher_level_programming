861a4b8d2a50f0fc7a670de3ef15686ef67646c1
jmugisha1 <j.mugisha1@alustudent.com>
2023-11-09 12:46:07 +0200

    python-more_data_structures

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for item in range(len(my_list)):
+        if new_list[item] == search:
+            new_list[item] = replace
+    return new_list
