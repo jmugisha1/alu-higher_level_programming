f9477c0b77782a55b80ae4c7b6e57fc8f82319ed
jmugisha1 <j.mugisha1@alustudent.com>
2023-11-10 12:23:32 +0200

    python-exceptions

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        final = a/b
+    except Exception as e:
+        final = None
+    finally:
+        print("Inside result: {}".format(final))
+        return final
---
+++ python-exceptions/git.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m 'python-exceptions'
+git push
