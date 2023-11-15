a56276edc15a4c5d3697402b1fde607cb26ce0c9
jmugisha1 <j.mugisha1@alustudent.com>
2023-11-15 22:26:49 +0200

    python-classes

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module to define a square class"""
+
+class Square:
+    """Class to define a square"""
+
+    def __init__(self, size):
+        """this is a comment"""
+
+        self.size = size
