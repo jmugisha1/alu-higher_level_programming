1fe19cda5242296b6337313356c1d122a97da890
jmugisha1 <j.mugisha1@alustudent.com>
2023-11-16 23:01:30 +0200

    python-classes

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module to define a square class"""
+
+
+class Square:
+    """Class to define a square"""
+
+    def __init__(self, size=0):
+        """this is a comment"""
+        if type(size) == int and size >= 0:
+            self.__size = size
+        elif type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+    
+    def area(self):
+        return self.__size * self.__size
