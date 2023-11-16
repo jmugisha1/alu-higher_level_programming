3075366bf31da63ba85f576200b865e66d855535
jmugisha1 <j.mugisha1@alustudent.com>
2023-11-16 22:37:11 +0200

    python-classes

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square:
 
     def __init__(self, size=0):
         """this is a comment"""
-        if type(size) is int and size > 0:
+        if type(size) is int and size >= 0:
             self.__size = size
         elif type(size) is not int:
             raise TypeError("size must be an integer")
