af7994c523ea3c7481bc138b68b1a258a8afeabb
jmugisha1 <j.mugisha1@alustudent.com>
2023-11-17 12:14:32 +0200

    python-classes

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,19 +7,19 @@ class Square:
 
     def __init__(self, size=0):
         """this is a comment"""
+        self.size = size
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        if type(value) == int and value >= 0:
+            self.__size =value
+        elif type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
         
-        @property
-        def size(self):
-            return self._size
-        
-        @size.setter
-        def size(self, value):
-            if type(value) == int and size >= 0:
-                self.__size = size
-            elif type(value) is not int:
-                raise TypeError("size must be an integer")
-            elif size < 0:
-                raise ValueError("size must be >= 0")
-                    
     def area(self):
         return self.__size * self.__size
