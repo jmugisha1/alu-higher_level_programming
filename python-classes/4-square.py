3065689985780dc74a92612bf576418c7e858d22
jmugisha1 <j.mugisha1@alustudent.com>
2023-11-17 13:08:36 +0200

    python-classes

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -12,7 +12,7 @@ class Square:
     @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         if type(value) == int and value >= 0:
@@ -21,6 +21,6 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        
+
     def area(self):
         return self.__size * self.__size
