e4b1c1e028e235328d6cbb9a51aab5a253960796
jmugisha1 <j.mugisha1@alustudent.com>
2023-11-17 20:52:12 +0200

    python-more_classes

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -52,5 +52,5 @@ class Rectangle:
             for i in range(0, self.__height):
                 for j in range(0, self.__width):
                     rect += "#"
-                rect += "#"
+                rect += "\n"
         return rect
