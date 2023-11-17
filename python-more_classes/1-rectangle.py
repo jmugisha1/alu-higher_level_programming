eef1902b1696975f67280cfc250b96b38df5bc32
jmugisha1 <j.mugisha1@alustudent.com>
2023-11-17 20:18:56 +0200

    python-more_classes

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module to create a rectangle class"""
+
+
+class Rectangle:
+    """Represents a rectangle"""
+
+    def width(self, width):
+        self.width = width
+    
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer") 
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+    
+    def height(self, height):
+        self.height = height
+    
+    @property
+    def height(self):
+        return self.__height
+    
+    @width.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer") 
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
