63d7ef041010cddeaf193a8eda7e3095df3e77eb
jmugisha1 <j.mugisha1@alustudent.com>
2023-11-29 16:17:38 +0200

    SQL_introduction

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,2 +1,11 @@
--- Show number of records with 89 as id
-SELECT COUNT(id) FROM first_table WHERE id = 89;
+-- Create table and insert values
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INTO second_table VALUES 
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
