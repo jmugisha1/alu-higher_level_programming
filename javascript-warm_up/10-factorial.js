1ac33a0d471551cb3d6eb8585da96e7c5917390b
jmugisha1 <j.mugisha1@alustudent.com>
2024-01-30 23:46:27 +0200

    commit

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 
-const num = parseInt (args[0]);
+const num = parseInt(args[0]);
 let factRes;
 
 if (num === NaN) {
-  console.log (factRes);
+  console.log(factRes);
 } else {
-  function fact (n) {
-    if (n === 0) {
+  function fact(n) {
+    if (n == 0) {
       return 1;
     } else {
-      return n * fact (n - 1);
+      return n * fact(n - 1);
     }
   }
-  factRes = fact (num);
+  factRes = fact(num);
   console.log(factRes);
 }
