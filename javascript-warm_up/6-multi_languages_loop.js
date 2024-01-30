cc38afb0615ac0b7b83df5f7e46d204d6dd06f90
jmugisha1 <j.mugisha1@alustudent.com>
2024-01-30 13:07:09 +0200

    commit

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < args.length; i++) {
-  console.log(el[i]);
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
 }
