eab69023d5f80de23bba11e77b76bbfb6687f4ac
jmugisha1 <j.mugisha1@alustudent.com>
2024-01-30 14:40:32 +0200

    commit

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,11 +4,10 @@ const num = parseInt(args[0]);
 
 if (num === undefined) {
   console.log('Missing size');
+} else if (num < 0) {
+  console.log();
 } else {
-  for (let rep = 0; rep <= num; rep++) {
-    for (let rep = 0; rep <= num; rep++) {
-      console.log('*');
-    }
-    console.log('');
+  for (let rep = 0; rep < num; rep++) {
+    console.log('X'.repeat(num));
   }
 }
