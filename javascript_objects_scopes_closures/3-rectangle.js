42f3ba93254142b77aa59d5a19e2641a39800d27
jmugisha1 <j.mugisha1@alustudent.com>
2024-02-03 01:03:16 +0200

    commit

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -9,7 +9,7 @@ class Rectangle {
   print() {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        console.log('*');
+        process.stdout.write('*');
       }
       console.log();
     }
