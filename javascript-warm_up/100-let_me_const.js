35494e89709201d2faeea8a2a2f59f14a9289cc7
jmugisha1 <j.mugisha1@alustudent.com>
2024-02-02 13:29:12 +0200

    commit

--- javascript-warm_up/100-let_me_const.js
+++ javascript-warm_up/100-let_me_const.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 let myVar = 333;
-module.export = myVar;
+module.exports = myVar;
--- javascript-warm_up/100-main.js
+++ javascript-warm_up/100-main.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 myVar = 89;
-let myVar = require('./100-let_me_const');
+require('./100-let_me_const');
 console.log(myVar);
