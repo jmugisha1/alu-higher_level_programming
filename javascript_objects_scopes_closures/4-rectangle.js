2b3b5907129aee475a3677b216dceb9675bcabf0
jmugisha1 <j.mugisha1@alustudent.com>
2024-02-03 13:31:30 +0200

    commit

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -15,24 +15,29 @@ class Rectangle {
     }
   }
 
-  rotate() {
-    [this.width, this.height] = [this.height, this.width];
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
+  double() {
+    let doubleHeight = this.height * 2;
+    let doubleWidth = this.width * 2;
+
+    for (let i = 0; i < doubleHeight; i++) {
+      for (let j = 0; j < doubleWidth; j++) {
         process.stdout.write('X');
       }
       console.log('');
     }
   }
 
-  double() {
-    (this.height = this.height * 2), (this.width = this.width * 2);
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
+  rotate() {
+    let rotateWidth = this.width;
+    let rotateHeight = this.height;
+    [rotateWidth, rotateHeight] = [rotateHeight, rotateWidth];
+    for (let i = 0; i < rotateHeight * 2; i++) {
+      for (let j = 0; j < rotateWidth * 2; j++) {
         process.stdout.write('X');
       }
       console.log('');
     }
+    console.log(this.height, this.width);
   }
 }
 module.exports = Rectangle;
