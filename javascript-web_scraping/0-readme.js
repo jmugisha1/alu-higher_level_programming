c212fcd5d582f2d4322b63fcc3a90b672082fc12
jmugisha1 <j.mugisha1@alustudent.com>
2024-02-09 13:44:03 +0200

    commit

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs')
 const filePath = process.argv[2];
-fs.readFileSync (filePath, 'utf-8', (err, data) => {
+fs.readFile (filePath, 'utf-8', (err, data) => {
     if (err) console.log(err)
     console.log(data)
 })
