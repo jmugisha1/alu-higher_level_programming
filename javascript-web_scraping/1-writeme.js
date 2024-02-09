595a803fe939d3818e69ada1f65e273fde96a105
jmugisha1 <j.mugisha1@alustudent.com>
2024-02-09 21:22:05 +0200

    commit

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,5 +2,7 @@
 const fs = require('fs');
 const filePath = process.argv.slice[2];
 fs.writeFile(filePath[0], filePath[1], 'utf-8', (err) => {
-  if (err) console.log(err);
+  if (err) {
+    console.log(err);
+  }
 });
